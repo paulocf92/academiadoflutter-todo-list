0c8d7a8d029404ebd4ffdf943179d97ade6f61e4
Paulo Filho <pcf.feat@gmail.com>
2024-04-26 10:34:51 -0300

    refactor: replace repetitive boilerplate with an encapsulated structure

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:todo_list_provider/app/core/database/sqlite_adm_connection.dart';
-import 'package:todo_list_provider/app/modules/auth/login/login_controller.dart';
-import 'package:todo_list_provider/app/modules/auth/login/login_page.dart';
+import 'package:todo_list_provider/app/modules/auth/auth_module.dart';
 import 'package:todo_list_provider/app/modules/splash/splash_page.dart';
 
 class AppWidget extends StatefulWidget {
@@ -32,22 +30,7 @@ class _AppWidgetState extends State<AppWidget> {
     return MaterialApp(
       title: 'Todo List Provider',
       routes: {
-        '/login': (_) => MultiProvider(
-              providers: [
-                Provider(create: (_) => 'Repository'),
-                Provider(create: (_) => 'Service'),
-                ChangeNotifierProvider(create: (_) => LoginController()),
-              ],
-              child: LoginPage(),
-            ),
-        '/register': (_) => MultiProvider(
-              providers: [
-                Provider(create: (_) => 'Repository'),
-                Provider(create: (_) => 'Service'),
-                ChangeNotifierProvider(create: (_) => LoginController()),
-              ],
-              child: LoginPage(),
-            ),
+        ...AuthModule().routes,
       },
       home: SplashPage(),
     );
