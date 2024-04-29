3e8aa45ad909e8378fe74c2efb065441a3203fed
Paulo Filho <pcf.feat@gmail.com>
2024-04-29 01:12:11 -0300

    feat: replace raw theme with a custom theme

---
+++ lib/app/core/ui/todo_list_ui_config.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TodoListUiConfig {
+  TodoListUiConfig._();
+
+  static ThemeData get theme => ThemeData(
+        useMaterial3: false,
+        textTheme: GoogleFonts.mandaliTextTheme(),
+        primaryColor: const Color(0xff5C77CE),
+        primaryColorLight: const Color(0xffABC8F7),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: const Color(0xff5C77CE),
+          ),
+        ),
+      );
+}
