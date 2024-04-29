4d7264e08c7112a216d60843ed402e3a6c9de1e2
Paulo Filho <pcf.feat@gmail.com>
2024-04-29 01:24:41 -0300

    chore: finish login page, add sign-in button with a package

--- lib/app/modules/auth/login/login_page.dart
+++ lib/app/modules/auth/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sign_in_button/sign_in_button.dart';
 import 'package:todo_list_provider/app/core/widget/todo_list_logo.dart';
 
 class LoginPage extends StatelessWidget {
@@ -19,8 +20,8 @@ class LoginPage extends StatelessWidget {
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    SizedBox(height: 40),
-                    TodoListLogo(),
+                    const SizedBox(height: 40),
+                    const TodoListLogo(),
                     Padding(
                       padding: const EdgeInsets.symmetric(
                           horizontal: 40, vertical: 20),
@@ -28,9 +29,9 @@ class LoginPage extends StatelessWidget {
                         child: Column(
                           children: [
                             TextFormField(),
-                            SizedBox(height: 20),
+                            const SizedBox(height: 20),
                             TextFormField(),
-                            SizedBox(height: 10),
+                            const SizedBox(height: 10),
                             Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
@@ -40,15 +41,51 @@ class LoginPage extends StatelessWidget {
                                 ),
                                 ElevatedButton(
                                   onPressed: () {},
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(10),
-                                    child: Text('Login'),
-                                  ),
                                   style: ElevatedButton.styleFrom(
                                     shape: RoundedRectangleBorder(
                                       borderRadius: BorderRadius.circular(20),
                                     ),
                                   ),
+                                  child: const Padding(
+                                    padding: EdgeInsets.all(10),
+                                    child: Text('Login'),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: const Color(0xffF0F3F7),
+                          border: Border(
+                            top: BorderSide(
+                                width: 2, color: Colors.grey.withAlpha(50)),
+                          ),
+                        ),
+                        child: Column(
+                          children: [
+                            const SizedBox(height: 30),
+                            SignInButton(
+                              Buttons.google,
+                              onPressed: () {},
+                              padding: const EdgeInsets.all(5),
+                              shape: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Text("Don't have an account?"),
+                                TextButton(
+                                  onPressed: () {},
+                                  child: const Text('Register'),
                                 ),
                               ],
                             ),
