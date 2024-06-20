c3633f24aba3a440fa0328268e36d8f1b0972b24
Paulo Filho <pcf.feat@gmail.com>
2024-06-19 21:38:56 -0300

    app: setup firebase auth config with flutterfire

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:todo_list_provider/app/app_module.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(AppModule());
 }
