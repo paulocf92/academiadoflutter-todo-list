776411082aef5097721ff5c38cdc0cde97339496
Paulo Filho <pcf.feat@gmail.com>
2024-06-22 06:57:56 -0300

    chore: encapsulate ChangeNotifier in a single class and have controllers extend from it

--- lib/app/modules/auth/register/register_controller.dart
+++ lib/app/modules/auth/register/register_controller.dart
@@ -1,36 +1,33 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:flutter/material.dart';
+import 'package:todo_list_provider/app/core/notifier/default_change_notifier.dart';
 import 'package:todo_list_provider/app/exception/auth_exception.dart';
 
 import 'package:todo_list_provider/app/services/user/user_service.dart';
 
-class RegisterController extends ChangeNotifier {
+class RegisterController extends DefaultChangeNotifier {
   final UserService _userService;
-  String? error;
-  bool success = false;
 
   RegisterController({required UserService userService})
       : _userService = userService;
 
   Future<void> registerUser(String email, String password) async {
     try {
-      error = null;
-      success = false;
-
+      showLoadingAndResetState();
       notifyListeners();
 
       final user = await _userService.register(email, password);
 
       if (user != null) {
         // success
-        success = true;
+        success();
       } else {
         // error
-        error = 'Error when registering user';
+        setError('Error when registering user');
       }
     } on AuthException catch (e) {
-      error = e.message;
+      setError(e.message);
     } finally {
+      hideLoading();
       notifyListeners();
     }
   }
