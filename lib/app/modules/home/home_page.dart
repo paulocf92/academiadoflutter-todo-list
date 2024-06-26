b96d3050eed67a47c4a93e5cc37ba79514ebe99f
Paulo Filho <pcf.feat@gmail.com>
2024-06-26 09:51:22 -0300

    chore: add custom header

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_provider/app/core/ui/theme_extensions.dart';
+import 'package:todo_list_provider/app/core/ui/todo_list_icons.dart';
 import 'package:todo_list_provider/app/modules/home/widgets/home_drawer.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,8 +10,19 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        iconTheme: IconThemeData(color: context.primaryColor),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(TodoListIcons.filter),
+            itemBuilder: (_) => [
+              const PopupMenuItem<bool>(child: Text('Show finished tasks')),
+            ],
+          )
+        ],
       ),
+      backgroundColor: const Color(0xFFFAFBFE),
       drawer: HomeDrawer(),
       body: Container(),
     );
