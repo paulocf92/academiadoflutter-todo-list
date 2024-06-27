07ecb219637f02b475675b9e6d2b2d9d42bb4f94
Paulo Filho <pcf.feat@gmail.com>
2024-06-27 16:29:19 -0300

    chore: flesh out new task page and add calendar button widget

--- lib/app/modules/tasks/task_create_page.dart
+++ lib/app/modules/tasks/task_create_page.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_provider/app/core/ui/theme_extensions.dart';
+import 'package:todo_list_provider/app/core/widget/todo_list_field.dart';
 import 'package:todo_list_provider/app/modules/tasks/task_create_controller.dart';
+import 'package:todo_list_provider/app/modules/tasks/widgets/calendar_button.dart';
 
 class TaskCreatePage extends StatelessWidget {
   final TaskCreateController _controller;
+  final _formKey = GlobalKey<FormState>();
 
   TaskCreatePage({super.key, required TaskCreateController controller})
       : _controller = controller;
@@ -11,9 +15,46 @@ class TaskCreatePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Task'),
+        backgroundColor: Colors.transparent,
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () => Navigator.of(context).pop(),
+            icon: Icon(Icons.close, color: Colors.black),
+          ),
+        ],
       ),
-      body: Container(),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        label: Text(
+          'Save task',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Create new task',
+                  style: context.titleStyle.copyWith(fontSize: 20),
+                ),
+              ),
+              const SizedBox(height: 30),
+              TodoListField(label: ''),
+              const SizedBox(height: 20),
+              CalendarButton(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/app/modules/tasks/widgets/calendar_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list_provider/app/core/ui/theme_extensions.dart';
+
+class CalendarButton extends StatelessWidget {
+  const CalendarButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Row(
+        children: [
+          const Icon(Icons.today, color: Colors.grey),
+          const SizedBox(width: 10),
+          Text('SELECT A DATE', style: context.titleStyle),
+        ],
+      ),
+    );
+  }
+}
