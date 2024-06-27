7c58af61574d75b514be7325f76ab96736a95577
Paulo Filho <pcf.feat@gmail.com>
2024-06-27 06:09:44 -0300

    chore: add day of week filter

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -4,6 +4,7 @@ import 'package:todo_list_provider/app/core/ui/todo_list_icons.dart';
 import 'package:todo_list_provider/app/modules/home/widgets/home_drawer.dart';
 import 'package:todo_list_provider/app/modules/home/widgets/home_filters.dart';
 import 'package:todo_list_provider/app/modules/home/widgets/home_header.dart';
+import 'package:todo_list_provider/app/modules/home/widgets/home_week_filter.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -42,6 +43,7 @@ class HomePage extends StatelessWidget {
                     children: [
                       HomeHeader(),
                       HomeFilters(),
+                      HomeWeekFilter(),
                     ],
                   ),
                 ),
