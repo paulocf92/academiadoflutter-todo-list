6b3e3db9a6d406d15a0de5a082ba4675a2707adc
Paulo Filho <pcf.feat@gmail.com>
2024-07-02 17:15:57 -0300

    chore: enable device-aware localization (effect can be seen in the date time picker)

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:todo_list_provider/app/core/database/sqlite_adm_connection.dart';
 import 'package:todo_list_provider/app/core/navigator/todo_list_navigator.dart';
 import 'package:todo_list_provider/app/core/ui/todo_list_ui_config.dart';
@@ -35,6 +36,14 @@ class _AppWidgetState extends State<AppWidget> {
       title: 'Todo List Provider',
       theme: TodoListUiConfig.theme,
       navigatorKey: TodoListNavigator.navigatorKey,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', 'US'),
+        Locale('pt', 'BR'),
+      ],
       routes: {
         ...AuthModule().routes,
         ...HomeModule().routes,
