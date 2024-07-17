265a60f4b85afc349b5643ca1cd1a3a26b5e8b85
Paulo Filho <pcf.feat@gmail.com>
2024-07-17 11:29:51 -0300

    chore: add description label to task filter enum

--- lib/app/models/task_filter_enum.dart
+++ lib/app/models/task_filter_enum.dart
@@ -3,3 +3,16 @@ enum TaskFilterEnum {
   tomorrow,
   week,
 }
+
+extension TaskFilterDescription on TaskFilterEnum {
+  String get description {
+    switch (this) {
+      case TaskFilterEnum.today:
+        return 'TODAY\'S TASKS';
+      case TaskFilterEnum.tomorrow:
+        return 'TOMORROW\'S TASKS';
+      case TaskFilterEnum.week:
+        return 'WEEK TASKS';
+    }
+  }
+}
